35ea3d8f9b9e222aab710e1939b421df3b9f4d6b
Eduardo Berzunza <eduardoberzunzal@gmail.com>
2020-08-15 17:46:32 -0500

    add docs

---
+++ docs/precache-manifest.de983de94157c77467318300af99c08e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60771ef8e2f83a14c2a488065fe824cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a73c8d67895b7202e5",
+    "url": "/static/css/main.3ba40464.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a73c8d67895b7202e5",
+    "url": "/static/js/main.f09ddd6e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
